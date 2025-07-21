4ac7a461e9269c95a59ffd119d7c85cc4d4e0879
vinay kumar <42743455+vk13041995@users.noreply.github.com>
2025-07-21 10:32:07 +0530

    Add files via upload

---
+++ project-A/src/main/java/com/example/project_A/ProjectAApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAApplication.class, args);
+	}
+
+}
